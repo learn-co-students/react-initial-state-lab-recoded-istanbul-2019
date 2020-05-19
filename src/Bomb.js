4749ba8a008142ee68494fa93f11567016802ed8
Omar Alithawi <oadnan100@gmail.com>
2020-05-19 16:59:50 +0300

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,42 @@
 // your Bomb code here!
+import React, { Component } from 'react'
+
+export class Bomb extends Component {
+    constructor(props){
+        super()
+        this.state = {
+           secondsLeft : props.initialCount
+        }
+    }
+
+    timer() {
+        this.myInterval = setInterval(() => {
+            this.setState(prevState =>({
+                secondsLeft : prevState.secondsLeft - 1,
+            }))
+        }, 1000);
+     
+    }
+
+    timerClear(){
+        clearInterval(this.myInterval);
+    }
+
+
+    render() {
+        let isFlag;
+        if(this.state.secondsLeft > 0){
+            isFlag =  `${this.state.secondsLeft} seconds left before I go boom!`
+        }else{
+            isFlag = `Boom!`
+        }
+        return (
+            <div>
+              {isFlag}
+            </div>
+        )
+    }
+}
+
+
+export default Bomb
