041fc0cf7eb1584aff888ac23ce670a216e3bdc7
basil-kawak <basilalisa0@gmail.com>
2020-05-19 18:35:33 +0300

    Done.

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,40 @@
+import React, { Component } from "react";
+
 // your ImageSlider code here!
+export default class ImageSlider extends Component {
+
+    constructor(props) {
+        super()
+        this.state = {
+            currentSlideIndex: 0,
+        }
+    }   
+
+    render() {
+        return (
+            <div className='basil'>I am on slide {this.state.currentSlideIndex}</div>
+        )
+    }
+
+}
+// // export defaultclass ImageSlider extends Component {
+ 
+// //     // we use the constructor to set the INITIAL STATE
+// //     constructor() {
+// //       super()
+// //       this.state = {
+// //         count: 0
+// //       }
+// //     }
+   
+// //     render() {
+// //       return (
+// //         <div>
+// //           {this.state.count}
+// //         </div>
+// //       )
+// //     }
+// //   }
+
+
+  
