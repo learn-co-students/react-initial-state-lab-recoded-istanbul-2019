0ae62339440a6a29a1aaf099873a7fa78ce358e1
lilas-saleh <lilas.ahmad.saleh@gmail.com>
2020-05-20 13:10:50 +0300

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,23 @@
 // your Bomb code here!
+import React from 'react';
+
+class Bomb extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+  render() {
+    if (this.state.secondsLeft === 0) {
+      return 'Boom!'
+
+    } else {
+      return `${this.state.secondsLeft}`
+    }
+  }
+}
+
+export default Bomb;
